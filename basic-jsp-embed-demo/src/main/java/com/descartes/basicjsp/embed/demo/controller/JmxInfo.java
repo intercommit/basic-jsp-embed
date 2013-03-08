dcae14a9a04aa4d3b28de369a33d3074d644885d
InterCommIT <fwiers@descartes.com>
2013-03-08 12:46:20 +0100

    Re-added license info.

--- basic-jsp-embed-demo/src/main/java/com/descartes/basicjsp/embed/demo/controller/JmxInfo.java
+++ basic-jsp-embed-demo/src/main/java/com/descartes/basicjsp/embed/demo/controller/JmxInfo.java
@@ -1,3 +1,21 @@
+/*  Copyright 2013 Descartes Systems Group
+*
+*  This file is part of the "BasicJspEmbedDemo" project hosted on https://github.com/intercommit/basic-jsp-embed
+*
+*  BasicJspEmbed is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Lesser General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  any later version.
+*
+*  BasicJspEmbedDemo is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Lesser General Public License for more details.
+*
+*  You should have received a copy of the GNU Lesser General Public License
+*  along with BasicJspEmbedDemo.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
 package com.descartes.basicjsp.embed.demo.controller;
 
 import javax.servlet.http.HttpServletRequest;
